fbe2540056f0cd532dd0295951773ff9c563f45c
Maila Keerus <mkeerus@klass3-19-l.intra.itcollege.ee>
2015-10-10 15:19:35 +0300

    praktikum 1

--- src/teema1/Peamurdja1_foor.java
+++ src/teema1/Peamurdja1_foor.java
@@ -1,5 +1,7 @@
 package teema1;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
 import lib.Foor;
 
 /**
@@ -17,5 +19,34 @@ import lib.Foor;
  * NB! Kui kirjutad new Foor(); siis IDE pakub sulle Foori importimise võimalust.
  * Kliki punase teksti peale ja vajuta alt+enter.
  */
-public class Peamurdja1_foor {
+public class Peamurdja1_foor extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Foor foor = new Foor();
+
+        int count=0;
+
+
+        while (count<5){
+            foor.vahetaPunast();
+            foor.paus(2);
+            foor.vahetaKollast();
+            foor.paus(1);
+            foor.kustutaKollane();
+            foor.kustutaPunane();
+            int count2=0;
+            while (count2<6) {
+                foor.vahetaRohelist();
+                foor.paus(0.5);
+                count2=count2+1;
+            }
+            foor.vahetaKollast();
+            foor.paus(1.5);
+            foor.vahetaKollast();
+            foor.vahetaPunast();
+            count=count+1;
+        }
+
+
+    }
 }
